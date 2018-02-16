207c35770d9b0b8a8336d6889987f808cf964451
rahuldcoder <rahulthakur02k@gmail.com>
2018-02-17 00:54:11 +0530

    adding lower_bound of binary search

---
+++ lower_bound.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(int arr[], int n, int key)
+{
+    int low = 0, high = n - 1, ans = -1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else if (arr[mid] < key)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+}
+
+int main()
+{
+    int arr[10] = {1, 2, 3, 3, 3, 3, 3, 4, 4, 5};
+    cout << lowerBound(arr, 10, 3) << endl;
+}
